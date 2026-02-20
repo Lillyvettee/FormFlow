fc4081cf00b2edc7fbd47b3bebb1e4115bd947ee
Lillyvettee <97841932+Lillyvettee@users.noreply.github.com>
2026-02-19 19:37:28 -0600

    Update supabase.ts

--- FinalFormFlow/supabase.ts
+++ FinalFormFlow/supabase.ts
@@ -3,8 +3,12 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
+// Delete this 
 if (!supabaseUrl || !supabaseAnonKey) {
-  throw new Error('Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY in environment variables')
+  throw new Error('Missing VITE_SUPABASE_URL...')
+}
+//if (!supabaseUrl || !supabaseAnonKey) {
+//  throw new Error('Missing VITE_SUPABASE_URL or VITE_SUPABASE_ANON_KEY in environment variables')
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
